8b7dce66899258c9686b870817e1ceb73956397b
paul-cruz <poljet12@gmail.com>
2020-06-12 22:41:55 -0500

    To stateful widget

--- tootor/lib/main.dart
+++ tootor/lib/main.dart
@@ -1,22 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tootor/src/pages/main_navegation_page.dart';
+import 'package:tootor/src/pages/signin_page.dart';
 import 'package:tootor/src/routes/routes.dart';
 import 'package:tootor/src/theme/theme.dart';
 
 void main() => runApp(TootorApp());
 
-class TootorApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TootorApp extends StatefulWidget {
+  TootorApp({Key key}) : super(key: key);
+
+  @override
+  _TootorAppState createState() => _TootorAppState();
+}
+
+class _TootorAppState extends State<TootorApp> {
+  Widget _rootPage = SignInPage();
+
+  Future<Widget> getRootPage() async =>
+      await FirebaseAuth.instance.currentUser() == null
+          ? SignInPage()
+          : MainNavegation();
+
+  @override
+  void initState() {
+    super.initState();
+    getRootPage().then((Widget page) {
+      setState(() {
+        _rootPage = page;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tootor',
-      initialRoute: '/signin',
+      //initialRoute: '/signin',
+      home: _rootPage,
       routes: buildAppRoutes(),
       theme: buildAppTheme(),
-      /*onGenerateRoute: (RouteSettings settings) {
-        return MaterialPageRoute(builder: (context) => AlertPage());
-      },*/
     );
   }
 }
