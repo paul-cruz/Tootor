26ceb1135e1a5e2a772b054106a062b33dd80242
paul-cruz <poljet12@gmail.com>
2020-06-14 02:03:13 -0500

    Design changes

--- tootor/lib/src/pages/signin_page.dart
+++ tootor/lib/src/pages/signin_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
